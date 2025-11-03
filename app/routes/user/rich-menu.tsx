26827b195e1250b4a058192cae255fc56a1b8598
449sabu <449sabu@gmail.com>
2025-11-04 07:50:21 +0900

    Enhance RichMenu component by adding file input for picture upload and utilizing useId for unique ID generation

--- app/routes/user/rich-menu.tsx
+++ app/routes/user/rich-menu.tsx
@@ -1,4 +1,7 @@
+import { useId } from 'react';
 import type { Route } from './+types/rich-menu';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
 
 export async function loader({ context }: Route.LoaderArgs) {
     const r2 = context.cloudflare.env.OGAWANOUEN_R2;
@@ -9,7 +12,14 @@ export async function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function RichMenu({ loaderData }: Route.ComponentProps) {
+	const pictureId = useId();
 	const { richMenu } = loaderData;
 	console.log(richMenu);
-	return <div>RichMenuPage</div>;
+	return (
+		<div>
+			RichMenuPage
+			<Label htmlFor="picture">Picture</Label>
+			<Input id={pictureId} type="file" name="picture"/>
+		</div>
+	);
 }
